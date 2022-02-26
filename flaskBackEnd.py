90fc552093636d1c354fd94f5d591e67c56c74bd
Brady Wu <zwu25@uci.edu>
2022-02-26 14:36:41 -0800

    webpage now displays what servers returns

--- flaskBackEnd.py
+++ flaskBackEnd.py
@@ -1,13 +1,15 @@
 from flask import Flask
+from flask import request
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-@app.route("/submittedURL",methods=["POST"])
-def update_difficulty():
-    global difficulty
-    difficulty = string_Difficult_to_int(request.form['difficulty'])
-    print(request.form['difficulty'])
+@app.route("/sendURL",methods=["POST"])
+def sendURL():
+    print(request.form['videoTitle'])
     return "200"
 
-print("testing")
