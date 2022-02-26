41e86980a035b60b85c4b338044d9ed203d33be4
Brady Wu <zwu25@uci.edu>
2022-02-26 15:50:20 -0800

    added in exception handling

--- flaskBackEnd.py
+++ flaskBackEnd.py
@@ -1,15 +1,29 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS
+import URLKey
+import getCaptions
+
 
 app = Flask(__name__)
 CORS(app)
-@app.route("/")
+
+fullString = ""
+
+@app.route("/getFullString")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return f"<p>{fullString}</p>"
 
 @app.route("/sendURL",methods=["POST"])
 def sendURL():
-    print(request.form['videoTitle'])
-    return "200"
+    VID = URLKey.getURLkey(request.form['videoTitle'])
+    if VID==1:
+        return "6969"
+    try:
+        fullTranscript = getCaptions.transcripter(VID)
+        global fullString
+        fullString = fullTranscript
+        return fullTranscript
+    except Exception as e:
+        return "6969"
 
