14600b60afe0cd884e326ebd2330cf5557e7b8ac
Brady Wu <zwu25@uci.edu>
2022-02-26 18:35:13 -0800

    website can now return summary of youtube videos based off link

---
+++ getSummary.py
@@ -0,0 +1,19 @@
+import requests
+def get_summary(api_key, user_url):
+    api_site = "https://api.smmry.com"
+    response = requests.get(api_site + '/&SM_API_KEY=' + api_key + '&SM_URL=' + user_url)
+    sm_response = response.json()
+    print(sm_response)
+    print(api_key)
+    if('sm_api_error' in sm_response):
+        return -1
+    
+
+    summary = sm_response['sm_api_content']
+    return summary
+
+if __name__ == "__main__":
+    # testing an usage example
+    # user_url = 'https://www.cbsnews.com/live-updates/russia-ukraine-invasion-war-donbas-kyiv-troops-today/'
+    # print(get_summary(user_url))
+    pass
